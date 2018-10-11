0d09f6d30c0168bfc128cb7318004122b04df671
Leandro Lopez <leandroe.lopez95@gmail.com>
2018-10-10 21:33:25 -0300

    Nuevo controlador de prueba

---
+++ pizeria/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pizeria.Controllers
+{
+    public class ProductController : Controller
+    {
+        //GET: Product
+        public ActionResult Index( int category=1)
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/ 
+    }
+}
